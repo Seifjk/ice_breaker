2df19f95744020be4186b02a787b1a6c7c3b4330
Seifjk <srfzxz@gmail.com>
2023-07-10 14:36:39 +0200

    day2

---
+++ curl_linkedin.py
@@ -0,0 +1,12 @@
+import requests
+
+api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+api_key = '4Sa7ws9huIgKyv_ciQ4WSQ'
+header_dic = {'Authorization': 'Bearer ' + api_key}
+params = {
+    'url': 'https://www.linkedin.com/in/seifeddineshili/',
+}
+response = requests.get(api_endpoint,
+                        params=params,
+                        headers=header_dic)
+response.json()
--- ice_breaker.py
+++ ice_breaker.py
@@ -1,7 +1,24 @@
-import os
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
 
-if __name__ == '__main__':
+information = """
+Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a business magnate and investor. He is the founder, CEO and chief engineer of SpaceX; angel investor, CEO and product architect of Tesla, Inc.; owner and CTO of Twitter; founder of the Boring Company; co-founder of Neuralink and OpenAI; and president of the philanthropic Musk Foundation. Musk is the wealthiest person in the world, with an estimated net worth, as of July 6, 2023, of around US$248 billion according to the Bloomberg Billionaires Index and $251.1 billion according to Forbes's Real Time Billionaires list, primarily from his ownership stakes in Tesla and SpaceX.[4][5][6]
+"""
+
+if __name__ == "__main__":
     print("Hello langchain!")
-    print(os.environ['OPENAI_API_KEY'])
 
-    
+    summary_template = """
+        given the Linkedin information {information} about a person from I want you to create:
+        1. a short summary
+        2. two interesting facts about them
+    """
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    print(chain.run(information=information))
