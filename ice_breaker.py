74b02a3f7657de662d1c3aad0584d32bf1e06bf7
Seifjk <srfzxz@gmail.com>
2023-07-13 17:40:07 +0200

    windows-support

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,6 +4,15 @@ from langchain.chains import LLMChain
 
 from third_parties.linkedin import scrape_linkedin_profile
 
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+# Now you can access your environment variables
+openai_api_key = os.getenv("OPENAI_API_KEY")
+proxycurl_api_key = os.getenv("PROXYCURL_API_KEY")
 # information = """
 # Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a business magnate and investor. He is the founder, CEO and chief engineer of SpaceX; angel investor, CEO and product architect of Tesla, Inc.; owner and CTO of Twitter; founder of the Boring Company; co-founder of Neuralink and OpenAI; and president of the philanthropic Musk Foundation. Musk is the wealthiest person in the world, with an estimated net worth, as of July 6, 2023, of around US$248 billion according to the Bloomberg Billionaires Index and $251.1 billion according to Forbes's Real Time Billionaires list, primarily from his ownership stakes in Tesla and SpaceX.[4][5][6]
 # """
