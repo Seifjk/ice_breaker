9f0a9877e8f9b992c53bf3f0870fe57dbba34ad8
Seifjk <srfzxz@gmail.com>
2023-07-17 12:06:54 +0200

    added linkedin tools and agents

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,6 +3,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 from dotenv import load_dotenv
 import os
@@ -19,6 +20,7 @@ proxycurl_api_key = os.getenv("PROXYCURL_API_KEY")
 
 if __name__ == "__main__":
     print("Hello langchain!")
+    linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco")
 
     summary_template = """
         given the Linkedin information {information} about a person from I want you to create:
@@ -32,9 +34,8 @@ if __name__ == "__main__":
     llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-
-
+    linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco")
     linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/heni-jalel-60b5b36/"
+        linkedin_profile_url=linkedin_profile_url
     )
     print(chain.run(information=linkedin_data))
